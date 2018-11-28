aea3e192d2047adf4cee2342f3e4b7fd29542e2e
Kat Michaela <k@gothfem.me>
2018-11-28 17:51:03 -0500

    validations and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :favorites
   has_many :conversations, through: :favorites
+  validates :username, presence: true, uniqueness: true, length: {in: 1..20}
+  validates :password, presence: true, confirmation: true, length: {in: 6..20}, on: :create
 
   def self.from_token_request request
     username = request.params["auth"] && request.params["auth"]["username"]
