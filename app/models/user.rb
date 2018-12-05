6c5905a35bf194528ac5ba421dd77adf2130c87f
Kat Michaela <k@gothfem.me>
2018-12-05 14:34:40 -0500

    real alphanumeric validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :created_conversations, class_name: :Conversation, foreign_key: "user_id"
   has_many :conversations, through: :favorites
-  validates :username, presence: true, uniqueness: true, length: {in: 1..20}, format: {with: /^[A-Za-z0-9]+$/}
+  validates :username, presence: true, uniqueness: true, length: {in: 1..20}, format: {with: /\A[a-zA-Z0-9]+\z/}
   validates :password, presence: true, confirmation: true, length: {in: 6..20}, on: :create
 
   def self.from_token_request request
